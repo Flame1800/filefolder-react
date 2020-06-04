dffd67abaa518d82e3cd0dca93c8f92fc3ec237b
Flame1800 <ruslan08022001@gmail.com>
2020-06-04 21:55:54 +0500

    Updates

---
+++ precache-manifest.97208cf8ee6d0d05bb2a790a57b374f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c10ca45392d8a8d4bc9653409e617d4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a4a88023b8c198b2d523",
+    "url": "./static/css/main.02e627a6.chunk.css"
+  },
+  {
+    "revision": "a7078ce2a0804b8b038d",
+    "url": "./static/js/2.0218271e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.0218271e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a88023b8c198b2d523",
+    "url": "./static/js/main.a8d23ce0.chunk.js"
+  },
+  {
+    "revision": "8f0a2a0018f4a7480b95",
+    "url": "./static/js/runtime-main.67c83c84.js"
+  },
+  {
+    "revision": "2022924574aa5f4b3ef4534429569de3",
+    "url": "./static/media/MaskGroup.20229245.jpg"
+  },
+  {
+    "revision": "b98ed864969bf00a9889e8d427522477",
+    "url": "./static/media/logo.b98ed864.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/filefolder-react/precache-manifest.7bd06ec9aaa6e3f704d43a3a58f2b2c5.js"
+  "./precache-manifest.97208cf8ee6d0d05bb2a790a57b374f1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/filefolder-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
