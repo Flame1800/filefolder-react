275e26a911fb7e4cb4476e96120e86d7817b0bd4
Flame1800 <ruslan08022001@gmail.com>
2020-06-11 11:32:06 +0300

    Updates

---
+++ precache-manifest.389a86b9b71417663f3dbea31504cf49.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d911552839cb77457291df31c073191e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "05034c32a0067af64aec",
+    "url": "./static/css/main.b8504fec.chunk.css"
+  },
+  {
+    "revision": "c4946f47ebc66303f0f4",
+    "url": "./static/js/2.fdec8643.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.fdec8643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05034c32a0067af64aec",
+    "url": "./static/js/main.ed035ad5.chunk.js"
+  },
+  {
+    "revision": "8f0a2a0018f4a7480b95",
+    "url": "./static/js/runtime-main.67c83c84.js"
+  },
+  {
+    "revision": "111bde5d123edb199a8aae1ae460ed48",
+    "url": "./static/media/header.111bde5d.png"
+  },
+  {
+    "revision": "da6c24d5acb3cbe07039246edfaeefcd",
+    "url": "./static/media/logo-smal.da6c24d5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.07423aea318e61bd617113271b988579.js"
+  "./precache-manifest.389a86b9b71417663f3dbea31504cf49.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
